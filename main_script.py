8938822566c00d3b30ac88df71312a4c73ec6901
Sakengali <sakengali.kazhiyev@nu.edu.kz>
2024-07-11 08:55:07 +0200

    fixed the wrong date

--- main_script.py
+++ main_script.py
@@ -34,7 +34,7 @@ def main():
             except Exception as error:
                 send_email_main(is_successful=False, error=error)
     else:           # all other months
-        if today == 17 or today == 30:
+        if today == 15 or today == 30:
             try:
                 download_data()
                 upload_data()
