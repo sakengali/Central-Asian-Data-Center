a70bc8f0b39e27b10786ef9b4e5d13c35e8c67b0
Sakengali <sakengali.kazhiyev@nu.edu.kz>
2024-07-31 15:11:33 +0200

    added sensor namedtuples

--- helpers.py
+++ helpers.py
@@ -2,12 +2,51 @@ import pandas as pd
 import os
 from upload_data_to_drive import get_date_folder_name
 from datetime import datetime
+from typing import NamedTuple, List
+import csv
 
 cwd = "/home/dhawal/Air Quality Analysis Central Asia/Central-Asian-Data-Center"
-level_folder = "Level 0"
+level_folder : str = "Level 0"
 date_folder_name = get_date_folder_name()
 
-def sensor_line(sensor_name, status):
+class Sensor(NamedTuple):
+    name : str
+    sensor_type : str
+    country : str
+    is_deployed : bool
+    location : str
+    owner : str
+    
+    def is_responding(self):
+        sensor_file_name = f"{self.name}-{date_folder_name[:8]}.csv"
+        df = pd.read_csv(f"{cwd}/Central Asian Data/{self.country}/{level_folder}/{date_folder_name}/{self.sensor_type}/{sensor_file_name}")
+
+        return not df.empty
+
+    def is_turned_off(self):
+        return self.is_deployed and (not self.is_responding)
+
+def get_sensors_info(country : str) -> List[Sensor]:
+    sensors : List[Sensor] = []
+    
+    with open(f"sensors_info/{country.lower()}_deployed_sensors.csv", mode='r', newline='') as file:
+        reader = csv.DictReader(file)
+        
+        for row in reader:
+            person = Sensor(
+                name = row['sensor_name'],
+                sensor_type=row['sensor_type'],
+                country = row['country'].upper(),
+                is_deployed=bool(int((row['is_deployed']))),
+                location=row['location'],
+                owner=row['owner'],
+            )
+            sensors.append(person)
+    
+    return sensors
+
+
+def sensor_line(sensor_name, status) -> str:
     if len(sensor_name) < 8:
         return f"{sensor_name}\t" + "\t"*(2) + f"{status}"
     elif len(sensor_name) < 16:
